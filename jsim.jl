4eef0e39d178aee58efd391f6f7656788fadaa00
Hersh Kumar <hershkumar13@gmail.com>
2023-12-17 20:49:17 -0500

    initial commit

---
+++ jsim.jl
@@ -0,0 +1,8 @@
+using GLMakie
+
+#= 
+Need to write the skeleton of the spin system
+Want to work with general interactions, if possible, but if not, then nearest-neighbor in 2D and 3D
+
+=#
+# need to figure out how to animate it
